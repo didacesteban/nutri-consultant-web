b4a9305598740f84a52d213836554e127ffe8427
Didac Esteban Andres <didacesteban@gmail.com>
2019-11-10 21:53:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="row">
+        <div className="col col-lg-4 col-md-4 col-xs-12 text-left">
+          nutri-consultant
+        </div>
+        <div className="col col-lg-8 col-md-8 hidde-xs text-right">
+          haz tu consulta | informate sobre nutrición
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
