6a1683cf0bc437c00d1cc08b2127d36a3f96abef
didac esteban <didacesteban@gmail.com>
2019-11-11 11:12:34 +0100

    separating menu in a component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import Menu from './components/menu/Menu.js';
 
 function App() {
   return (
-    <div className="App">
-      <div className="row App-header">
-        <div className="col col-lg-4 col-md-4 col-xs-12 text-left">
-          <span className="App-title">Nutriconsultant.</span>
-        </div>
-        <div className="col col-lg-8 col-md-8 hidde-xs text-right">
-          <span className="App-menu-item">Informate sobre nutrición</span>
-          <span className="App-menu-item">¿Quienes somos?</span>
-          <button className="App-menu-button">haz tu consulta</button>
-        </div>
-      </div>
+    <div>
+      <Menu />
     </div>
   );
 }
---
+++ src/components/menu/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Menu.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+function Menu() {
+  return (
+    <div className="Menu">
+      <div className="row Menu-header">
+        <div className="col col-lg-4 col-md-4 col-xs-12 text-left">
+          <span className="Menu-title">Nutriconsultant.</span>
+        </div>
+        <div className="col col-lg-8 col-md-8 hidde-xs text-right">
+          <span className="Menu-menu-item">Informate sobre nutrición</span>
+          <span className="Menu-menu-item">¿Quienes somos?</span>
+          <button className="Menu-menu-button">haz tu consulta</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
