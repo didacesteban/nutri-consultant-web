625b1fa461647545cd3fc2595062930c43b5c70e
didac esteban <didacesteban@gmail.com>
2019-11-12 11:53:45 +0100

    refactor

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Menu from '../../components/menu/Menu';
-import './Home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment, faHeadset, faUtensils } from '@fortawesome/free-solid-svg-icons'
 
